bbad10e239928b2e25c8fffe712dc21e231ae7ef
navnishan <navnishann@gmaol.com>
2026-01-17 23:47:55 +0200

    Add Position and TaskType models with migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="Position name"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Position"
+        verbose_name_plural = "Positions"
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class TaskType(models.Model):
+    name = models.CharField(
+        max_length = 100,
+        unique = True,
+        verbose_name = "Task Type name"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Task Type"
+        verbose_name_plural = "Task Types"
+
