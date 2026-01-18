cd0d6a1793d0f680424abe540314f171766d964f
navnishan <navnishann@gmaol.com>
2026-01-18 17:27:03 +0200

    Register models in Django Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Position, Worker
 
-# Register your models here.
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ['id','name']
+    search_fields = ['name']
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = [
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active',
+        'is_superuser'
+    ]
+    list_filter = ['position','is_staff','is_active']
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Information', {'fields': ('position',)}),
+    )
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TaskType
 
 # Register your models here.
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
