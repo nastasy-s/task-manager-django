1cc7828074d7d728d7ab1a3e1dad6b31369eac56
navnishan <navnishann@gmaol.com>
2026-01-19 20:53:08 +0200

    Add task detail view with dynamic URLs

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.task_list, name='task-list'),
+    path('<int:pk>/', views.task_detail, name='task-detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Task
 
 # Create your views here.
 def task_list(request):
-
     tasks = Task.objects.all()
-
     context = {
         'tasks': tasks,
     }
-
     return render(request, 'tasks/task_list.html', context)
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    context = {'task': task}
+    return render(request, 'tasks/task_detail.html', context)
